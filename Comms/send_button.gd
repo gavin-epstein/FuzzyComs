b11a7141ccbbcd6265cd03a622ccba6fae807ca1
gavin-epstein <wreakjoy@gmail.com>
2026-02-12 14:29:12 -0500

    scrambler for both A and B versions done

--- Comms/send_button.gd
+++ Comms/send_button.gd
@@ -2,19 +2,31 @@ extends Button
 
 var messageEntry:TextEdit
 var display
-var colors = ["red","white", "black", "green","yellow", "blue"]
+var colors = ["red","orange","white", "black", "green","yellow", "blue","purple", "pink", "brown", "grey", "gray","color","colours"]
+var shapes = ["circle", "triangle", "square", "cross", "pentagon","diamond", "rectangle", "rhombus", "quadrilateral", "oval", "ellipse", "star","shape"]
+var numbers = ["number","1","2","3","4","5","6","7","8","9","0","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen", "fourteen","fifteen","sixteen","seventeen", "eighteen", "nineteen", "twenty", "thirty","fourty", "forty","fifty", "sixty","seventy","eighty","ninety","hundred","thousand"]
+var level3list
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	messageEntry = $"../TextureRect/MessageEntry"
 	display = $"../MessageDisplay"
-
-
+	level3list = colors+shapes+numbers
+	level3list.sort_custom(lengthcomp)
+	
 
 func encode(message:String)-> String:
 	if globalNode.level ==1:
 		return message
-	elif globalNode.level ==2 or globalNode.level ==2 :
+	elif globalNode.level ==2 or globalNode.level ==3 :
+		if globalNode.version == "A":
+			return replaceFromList(message, level3list,"▒▒▒")
+		else:
+			var arr = message.split()
+			for i in range(0,len(arr),2):
+				arr[i] = "."
+			return  "".join(arr)
+	elif globalNode.level == 4:
 		return message
 	else:
 		return "Error Unknown Level"
@@ -48,4 +60,24 @@ func _http_request_completed(_result, response_code, headers, body):
 		_on_pressed() #try again
 	elif response["message"] == "Failed to Send":
 		pass
-	
+#assumes list is sorted by string length small to large	
+func replaceFromList(input:String, list:Array, replacewith:String)->String:
+	if len(list)==0:
+		return input
+	var replacelen = len(replacewith)
+	var i = 0
+	while i < len(input):
+		var curlen = len(list[0])
+		var sub = input.substr(i,curlen)
+		for word in list:
+			var wordlen = len(word)
+			if wordlen>curlen:
+				curlen = wordlen
+				sub = input.substr(i,curlen)
+			if word.to_lower()==sub.to_lower():
+				input = input.substr(0,i)+replacewith+input.substr(i+curlen)
+		i+=1
+	return input
+
+func lengthcomp(a,b)->bool:
+	return len(a)<len(b)
--- global.gd
+++ global.gd
@@ -4,4 +4,5 @@ static var code
 static var playerType #options: Ship, Station
 static var level = 1 #1: intro, 2: cutscene, 3: panel, 4: thanksforplaying
 static var otherCode
+static var version = "A"
 signal levelChanged

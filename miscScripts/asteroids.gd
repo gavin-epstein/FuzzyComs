1219848a0d3a4af69d888e0706f76b4d00f43f70
gavin-epstein <wreakjoy@gmail.com>
2026-03-19 17:52:30 -0400

    cutscene progress

--- miscScripts/asteroids.gd
+++ miscScripts/asteroids.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
-@export var speed = 5
+@export var speed:float = 5
 
 func _process(delta: float) -> void:
-	translate(Vector3(-1*speed*delta,0,0))
+	position+=(Vector3(-1*speed*delta,0,0))
 	if position.x < -100:
 		position.x = 300
